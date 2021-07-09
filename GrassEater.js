8f8bdf4b88cb415891863a9560b1af1cb59b8d2e
bellahs <bella.h.s@tumo.org>
2021-07-09 18:19:42 +0400

    coment

--- GrassEater.js
+++ GrassEater.js
@@ -1,42 +1,22 @@
-class GrassEater {
+class GrassEater extends LivingCreature {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        super(x, y)
         this.energy = 8;
-        this.multiply = 0
         this.directions = [];
-    }
 
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
 
-    chooseCell(character) {
+    }
+    chooseCell(ch) {
         this.getNewCoordinates()
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
+        return super.chooseCell(ch)
+        
     }
+
+
+
 
     mul() {
+    
         this.multiply++;
         var emptyCells = this.chooseCell(0);
         var newCell = random(emptyCells);
@@ -58,6 +38,7 @@ class GrassEater {
         var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
 
         if (newCell && this.energy >= 0) {
+            console.log(111111111);
             var newX = newCell[0]
             var newY = newCell[1]
             matrix[newY][newX] = matrix[this.y][this.x]
